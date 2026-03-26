b997044a70ae924edc104bb340a15886506d3217
Zain Yehia <zainyehia1@gmail.com>
2026-03-25 20:36:24 -0400

    Force remove hello_cargo folder

--- hello_cargo/target/debug/deps/hello_cargo-a9d44323681521d9.d
+++
@@ -1,5 +0,0 @@
-/home/zainyehia/rust/hello_cargo/target/debug/deps/hello_cargo-a9d44323681521d9.d: src/main.rs
-
-/home/zainyehia/rust/hello_cargo/target/debug/deps/libhello_cargo-a9d44323681521d9.rmeta: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/zainyehia/rust/hello_cargo/target/debug/hello_cargo: /home/zainyehia/rust/hello_cargo/src/main.rs
